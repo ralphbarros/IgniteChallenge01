091f0b94732cebb5703f3e1d0bc4233f856354fc
ralphbarros <ralphbarros@gmail.com>
2021-05-03 22:58:13 -0300

    first day

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
+//create users
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+  const newUser = {
+      id: uuidv4(),
+      name,
+      username,
+      todos: []
+    }
+    
+users.push(newUser);
+return response.json(  newUser  );
 });
+
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
